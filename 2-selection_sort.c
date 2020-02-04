f50a2f238b75ff956c1b4b9f6dac769ebfe39d75
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-04 06:11:07 +0000

    add task 2 selection sort an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,1 +1,34 @@
 #include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @array: the poiter to the array
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+int tmp, tmp_2;
+if (!array)
+{
+return;
+}
+for (size_t i = 0; i < size - 1; i++)
+{
+tmp = i;
+for (size_t j = i + 1; j < size; j++)
+{
+if (array[j] < array[tmp])
+{
+tmp = j;
+}
+}
+if (array[i] != array[tmp])
+{
+tmp_2 = array[i];
+array[i] = array[tmp];
+array[tmp] = tmp_2;
+print_array(array, size);
+}
+}
+}
